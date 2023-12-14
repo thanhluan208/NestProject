a62f8d79b693d46e3038e9edc84c890622f75c3f
thanhluan208 <skthunte33@gmail.com>
2023-12-14 11:25:50 +0700

    remove cert

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from 'fs';
-
-const httpsOptions = {
-  key: fs.readFileSync('secrets/cert.key'),
-  cert: fs.readFileSync('secrets/cert.crt'),
-};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
